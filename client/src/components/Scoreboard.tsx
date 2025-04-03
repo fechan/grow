2fa83b27b7ff9526da4d648ab21fe0afcc116149
fechan <56131910+fechan@users.noreply.github.com>
2025-04-03 02:44:40 -0700

    color scoreboard

--- client/src/components/Scoreboard.tsx
+++ client/src/components/Scoreboard.tsx
@@ -1,5 +1,12 @@
 import { Scores } from "../../../types/game";
 
+const colors = [
+  { normal: 'bg-red-600/10', turn: 'bg-red-600/80', border: 'border-red-600' },
+  { normal: 'bg-blue-600/10', turn: 'bg-blue-600/80', border: 'border-blue-600' },
+  { normal: 'bg-green-600/10', turn: 'bg-green-600/80', border: 'border-green-600' },
+  { normal: 'bg-yellow-600/10', turn: 'bg-yellow-600/80', border: 'border-yellow-600' },
+];
+
 interface ScoreboardProps {
   players: string[],
   scores: Scores,
@@ -14,14 +21,15 @@ export function Scoreboard({ players, scores, currentPlayer }: ScoreboardProps) 
           <section
             key={ player }
             className={
-              "border rounded p-3 text-center w-full md:w-50" +
-              ( player === currentPlayer ? " bg-yellow-500/30 " : "" )
+              "border rounded p-3 text-center w-full md:w-50 " +
+              ( colors[players.indexOf(player)].border ) + " " +
+              ( player === currentPlayer ? colors[players.indexOf(player)].turn : colors[players.indexOf(player)].normal )
             }
           >
             <h1 className="font-bold">
               { player }
               &nbsp;
-              ({ ['red', 'blue', 'green', 'yellow'][players.indexOf(player)] })
+              <span className="font-normal">({ ['red', 'blue', 'green', 'yellow'][players.indexOf(player)] })</span>
             </h1>
             <p>Score: { scores[player] || 0 }</p>
           </section>
