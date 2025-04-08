c5e1903e7a8072bd02f66963250dee22801d3021
fechan <56131910+fechan@users.noreply.github.com>
2025-04-07 22:07:11 -0700

    redirect to client if server accessed directly

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const { Server } = require("socket.io");
 
 const io = new Server(server, {
   cors: {
-    origin: ['https://fredchan.org', 'https://frederick.seattle.wa.us']
+    origin: ["https://fredchan.org", "https://frederick.seattle.wa.us"]
   }
 });
 
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 const GameController = require("./gameController");
 new GameController(io);
 
-// app.use("/", express.static("./client/"));
+app.use("/", (req, res) => res.redirect("https://fredchan.org/grow"));
 
 app.get("/up", (req, res) => {
   res.send("Grow server is up and running!");
